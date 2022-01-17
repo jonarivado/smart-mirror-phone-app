b2891c514d83a4e3a067d0b1bf7f2556b9685051
jonarivado <jonathan.dotzler@gmail.com>
2022-01-17 20:50:34 +0100

    added angular CDK for drag&drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -16,7 +16,8 @@ import { ClockComponent } from './components/widgets/clock/clock.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,49 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  userData?: any = {};
-  userComponents?: any;
-
-  if(): void {
-    console.log(this.userComponents);
+  
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.userComponents, event.previousIndex, event.currentIndex);
   }
 
-  constructor() {
-    this.userData = {
-      "name": "Anna",
-      "birthday": "1920-01-01T00:00:0.000Z",
-      "mailAddress": "anna.sulzer@shinternet.ch"
-    };
+  
+  userData = {
+    "name": "Anna",
+    "birthday": "1920-01-01T00:00:0.000Z",
+    "mailAddress": "anna.sulzer@shinternet.ch"
+  };
 
-    this.userComponents = [
-      {
-          "id": "weather",              // response depends on the component type, feel free to change 
-          "x": 0,
-          "y": 0,
-          "size": [2, 1],
-          "locationString": "Dübendorf"
-      },
-      {
-          "id": "clock",
-          "x": 0,
-          "y": 1,
-          "size": [2, 1],
-          "clockType": "analog"
-      }
-  ];
+  userComponents = [
+    {
+        "id": "weather",              // response depends on the component type, feel free to change 
+        "x": 0,
+        "y": 0,
+        "size": [2, 1],
+        "locationString": "Dübendorf"
+    },
+    {
+        "id": "clock",
+        "x": 0,
+        "y": 1,
+        "size": [2, 1],
+        "clockType": "analog"
+    }
+];
+
+
+
+  constructor() {
    }
 
   ngOnInit(): void {
     
   }
-
-  
 
   
 
