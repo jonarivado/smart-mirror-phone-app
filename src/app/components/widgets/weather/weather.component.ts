23bc2f3f89bd44a2244511ef025ffb40dc1880bb
jonarivado <jonathan.dotzler@gmail.com>
2022-01-23 11:35:56 +0100

    added whacky weather icons & functionality

--- src/app/components/widgets/weather/weather.component.ts
+++ src/app/components/widgets/weather/weather.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { delay, share } from 'rxjs/operators';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  weatherData!: Observable<any>;
+  weatherIcon!: string;
+
+  constructor(private http:HttpClient) { 
+    this.weatherData = this.http.get('https://api.open-meteo.com/v1/forecast?latitude=52.5235&longitude=13.4115&daily=temperature_2m_max,temperature_2m_min&current_weather=true&timezone=Europe%2FBerlin')
+    .pipe(share());
+  }
 
   ngOnInit(): void {
   }
