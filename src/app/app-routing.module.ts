8919084934364d74e88219bdc89884d892ec3163
jonarivado <jonathan.dotzler@gmail.com>
2022-01-26 15:39:04 +0100

    added very sporadic weather settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherSettingsComponent } from './components/component-settings/weather-settings/weather-settings.component';
 
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { SettingsComponent } from './components/settings/settings.component';
@@ -7,6 +8,7 @@ import { SettingsComponent } from './components/settings/settings.component';
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
   {path: 'settings', component: SettingsComponent},
+  {path: 'weathersettings', component: WeatherSettingsComponent},
 ];
 
 @NgModule({
---
+++ src/app/components/component-settings/weather-settings/weather-settings.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WeatherSettingsComponent } from './weather-settings.component';
+
+describe('WeatherSettingsComponent', () => {
+  let component: WeatherSettingsComponent;
+  let fixture: ComponentFixture<WeatherSettingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ WeatherSettingsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WeatherSettingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
